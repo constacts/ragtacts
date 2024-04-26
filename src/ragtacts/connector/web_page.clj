cb431dcb2340028e18187ff520b8059bbff0de0d
Todd Eunmin Kim <eunmin@constacts.com>
2024-04-26 21:42:17 +0900

    support last-modified, etag http connector

--- src/ragtacts/connector/web_page.clj
+++ src/ragtacts/connector/web_page.clj
@@ -1,16 +1,19 @@
 (ns ragtacts.connector.web-page
   (:require [clojure.string :as str]
             [hato.client :as http]
+            [overtone.at-at :as at]
             [ragtacts.connector.base :refer [Connector empty-change-log-result
                                              make-change-log
                                              make-change-log-result]]
-            [ragtacts.document-loader.base :refer [load-doc]]
+            [ragtacts.document-loader.base :refer [load-doc make-doc]]
             [ragtacts.document-loader.html :refer [make-html-loader]]
             [ragtacts.document-loader.text :refer [make-text-loader]]
-            [ragtacts.logging :as log]
-            [overtone.at-at :as at]))
+            [ragtacts.logging :as log]))
 
-(defrecord WebPageConnector [url interval pool !handler]
+(defn- html? [headers]
+  (str/starts-with? (get headers "content-type") "text/html"))
+
+(defrecord WebPageConnector [url interval pool !handler last-change]
   Connector
   (connect [_ callback]
     (reset! !handler
@@ -20,14 +23,24 @@
                (log/debug "Start WebPageConnector" url)
                (let [client (http/build-http-client {:connect-timeout 10000
                                                      :redirect-policy :always})
-                     {:keys [status headers body]} (http/get url {:http-client client})
-                     loader (cond
-                              (str/starts-with? (get headers "content-type") "text/html") (make-html-loader {})
-                              :else (make-text-loader {}))
-                     change-log-result (if (= status 200)
-                                         (make-change-log-result [(make-change-log
-                                                                   {:type :create
-                                                                    :doc (load-doc loader url body)})])
+                     request-headers (cond-> {}
+                                       (:last-modified @last-change) (assoc "if-modified-since" (:last-modified @last-change))
+                                       (:etag @last-change) (assoc "if-none-match" (:etag @last-change)))
+                     {:keys [status headers body]} (http/get url {:http-client client
+                                                                  :headers request-headers})
+                     last-modified (get headers "last-modified")
+                     etag (get headers "etag")
+                     change-log-result (case status
+                                         200 (let [loader (cond
+                                                            (html? headers) (make-html-loader {})
+                                                            :else (make-text-loader {}))]
+                                               (swap! last-change assoc :last-modified last-modified :etag etag)
+                                               (make-change-log-result [(make-change-log
+                                                                         {:type :create
+                                                                          :doc (load-doc loader url body)})]))
+                                         404 (make-change-log-result [(make-change-log
+                                                                       {:type :delete
+                                                                        :doc (make-doc url nil)})])
                                          empty-change-log-result)]
                  (callback {:type :complete :change-log-result change-log-result})))
              pool)))
@@ -44,4 +57,7 @@
 (defn make-web-page-connector [opts]
   (map->WebPageConnector (merge opts {:!handler (atom nil)
                                       :pool (at/mk-pool)
-                                      :interval (* 1000 10)})))
+                                      :interval (* 1000 10)
+                                      :last-change (atom
+                                                    {:last-modified nil
+                                                     :etag nil})})))
