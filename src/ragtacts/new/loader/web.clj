9a62d2ddc8bf7c7cb257575b12e4db01ea4cfeed
Todd Eunmin Kim <eunmin@constacts.com>
2024-05-15 19:23:41 +0900

    add web text loader

--- src/ragtacts/new/loader/web.clj
+++ src/ragtacts/new/loader/web.clj
@@ -1,3 +1,19 @@
-(ns ragtacts.new.loader.web)
+(ns ragtacts.new.loader.web
+  (:require [clojure.string :as str]
+            [hato.client :as http])
+  (:import [dev.langchain4j.data.document Document]
+           [dev.langchain4j.data.document.transformer HtmlTextExtractor]))
 
-(defn web-texts [url])
+(defn web-text [url]
+  (let [client (http/build-http-client {:connect-timeout 10000
+                                        :redirect-policy :always})
+        {:keys [status headers body]} (http/get url {:http-client client})]
+    (when (= status 200)
+      (when (str/starts-with? (get headers "content-type") "text/html")
+        (let [^Document doc (.transform (HtmlTextExtractor.) (Document/from body))]
+          (.text doc))))))
+
+(comment
+  (web-text "https://aws.amazon.com/what-is/retrieval-augmented-generation/")
+  ;;
+  )
--- src/ragtacts/new/main.clj
+++ src/ragtacts/new/main.clj
@@ -1,15 +1,15 @@
 (ns ragtacts.new.main
   (:require [ragtacts.new.core :refer [ask vector-store save search prompt embed]]
             [ragtacts.new.vector-store.milvus :refer [milvus]]
+            [ragtacts.new.loader.web :refer [web-text]]
             [ragtacts.new.embedding.open-ai :refer [open-ai-embedding]]
             [clojure.string :as str]))
 
 (comment
-  (ask "ragtacts는 무엇인가요?")
 
-  (let [question "ragtacts는 무엇인가요?"
+  (let [question "What is RAG?"
         db (vector-store {:db (milvus {:collection "test6"})})]
-    (save db ["ragtacts로 llm 응용 프로그램을 쉽게 만들 수 있습니다."])
+    (save db [(web-text "https://aws.amazon.com/what-is/retrieval-augmented-generation/")])
     (ask (prompt "You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
 Question: {{ question }}
 Context: {{ context }} 
