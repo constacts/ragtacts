da176e8f50c10b378ca95afd9215115d57c39017
Eunmin Kim <telepopsound@gmail.com>
2024-07-15 15:28:04 +0900

    prep 0.5.0

--- build.clj
+++ build.clj
@@ -6,7 +6,7 @@
             [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'com.constacts/ragtacts)
-(def version "0.4.1")
+(def version "0.5.0-SNAPSHOT")
 #_; alternatively, use MAJOR.MINOR.COMMITS:
   (def version (format "1.0.%s" (b/git-count-revs nil)))
 (def main-cls (string/join "." (filter some? [(name lib) "main"])))
